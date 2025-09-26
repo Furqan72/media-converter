d1c1216d68d5f39f74c585a57916d8acae7b6f6e
Furqan72 <ranafurqanhaider5@gmail.com>
2025-09-26 22:11:04 +0500

    header footer changes

--- video-converter/src/components/Footer.vue
+++ video-converter/src/components/Footer.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="bg-[#f2f2f2] px-28 py-14">
-    <div class="grid grid-cols-10">
-      <div v-for="(list, index) in lists" :key="index" class="mx-4">
-        <p class="pb-3 font-semibold text-black">{{ list.listName }}</p>
-        <ul class="text-[#b53836] [&>*]:py-1">
-          <li>{{ list.listContent[0].name1 }}</li>
-          <li>{{ list.listContent[0].name2 }}</li>
-        </ul>
+  <div class="bg-[#f2f2f2] px-4 py-14 comd:px-20 md:px-28">
+    <div class="grid md:grid-cols-5 lg:grid-cols-10">
+      <div class="md:col-span-3 md:flex md:flex-wrap lg:col-span-4 maxmd:grid maxmd:grid-cols-2">
+        <div v-for="(list, index) in lists" :key="index" class="mx-4 mb-5 maxmd:text-center">
+          <p class="font-semibold text-black md:pb-2">{{ list.listName }}</p>
+          <ul class="text-[#b53836] [&>*]:py-1">
+            <!-- <li>{{ list.listContent[0].name1 }}</li>
+          <li>{{ list.listContent[0].name2 }}</li> -->
+            <li v-html="list.listContent[0].name1"></li>
+            <li v-html="list.listContent[0].name2"></li>
+          </ul>
+        </div>
       </div>
-      <div class="col-span-6 text-end">
-        <p>Lorem, ipsum dolor amet.</p>
+      <div class="text-center md:col-span-2 md:text-end lg:col-span-6 maxmd:pt-5 maxmd:text-15px">
+        <p class="text-black">Lorem, ipsum dolor amet.</p>
         <p class="text-gray-400">Lorem ipsum dolor consectetur.</p>
       </div>
     </div>
-    <p class="mb-10 mt-14 text-center text-2xl font-semibold text-gray-400">We've already converted <span class="font-bold text-gray-500">1,672,395,206 files</span> with a total size of <span class="font-bold text-gray-500">12,972 TB.</span></p>
+    <p class="mb-7 mt-10 text-center text-xl font-semibold text-gray-400 md:mb-10 md:mt-14 md:text-2xl">We've already converted <span class="font-bold text-gray-500">1,672,395,206 files</span> with a total size of <span class="font-bold text-gray-500">12,972 TB.</span></p>
   </div>
 </template>
 
@@ -25,7 +29,7 @@ const lists = ref([
     listName: 'Company',
     listContent: [
       {
-        name1: 'About Us',
+        name1: 'About&nbsp;Us',
         name2: 'Security',
       },
     ],
@@ -52,7 +56,7 @@ const lists = ref([
     listName: 'Contact',
     listContent: [
       {
-        name1: 'Contact Us',
+        name1: 'Contact&nbsp;Us',
         name2: 'Imprint',
       },
     ],
